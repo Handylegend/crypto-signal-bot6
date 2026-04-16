f1749b7537ddf24d8446fb3239dc3ef0913434b2
Handylegend <127908548+Handylegend@users.noreply.github.com>
2026-04-16 18:03:42 -0400

    Remove WEBHOOK_URL validation
    Remove check for unset WEBHOOK_URL.

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import os
 
 WEBHOOK_URL = "https://discordapp.com/api/webhooks/1494452919678795776/kBiWgOnHsi3QcRDkE28lMsB20X9RSgOvO4Z3OGS7myk9DnNbcs-92z7-D4rnaD6IT0Tx"
 
-if not WEBHOOK_URL:
-    raise ValueError("WEBHOOK_URL 没有设置")
-
 SYMBOL = "BTC/USDT"
 TIMEFRAME = "5m"
 
